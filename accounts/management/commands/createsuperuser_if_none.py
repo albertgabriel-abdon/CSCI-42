0c90610fedcb15e13e73c9c17953d4445da88288
albertgabrielabdon <157718121+albertgabrielabdon@users.noreply.github.com>
2024-05-25 20:38:56 +0800

    Add files via upload

---
+++ accounts/management/commands/createsuperuser_if_none.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Create a superuser if none exists'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(is_superuser=True).exists():
+            username = 'albert'
+            email = 'albert.abdon@student.ateneo.edu'
+            password = 'albert'
+            
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created with username: albert and password: albert'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Superuser already exists'))
+
+
