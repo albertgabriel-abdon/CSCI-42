d993e8e29a7b04c79add6da912c2fbc42f73e92a
albertgabriel-abdon <166241481+albertgabriel-abdon@users.noreply.github.com>
2025-03-07 07:59:45 +0800

    added recipedetail view in urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleCreateView, ArticleGalleryView
+from .views import  (RecipeDetailView, ArticleListView, 
+                    ArticleDetailView, ArticleUpdateView, 
+                    ArticleDeleteView, ArticleCreateView, 
+                    ArticleGalleryView
+                    )
 
 urlpatterns = [
     path('blog/articles',ArticleListView.as_view(), name='article_site'),
@@ -8,5 +12,6 @@ urlpatterns = [
     path('blog/article/add', ArticleCreateView.as_view(), name='create'),
     path('blog/article/<int:pk>/delete', ArticleDeleteView.as_view(), name='delete'),
     path('blog/article/gallery', ArticleGalleryView.as_view(), name='gallery'),
+    path('blog/recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
 app_name = 'blog'
