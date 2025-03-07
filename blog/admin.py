1b233bd3fae5c1fb55c5113fd2300d6273772544
albertgabriel-abdon <166241481+albertgabriel-abdon@users.noreply.github.com>
2025-03-07 08:03:37 +0800

    added recipe admin to track

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article, ArticleCategory, Comment
+from .models import Article, ArticleCategory, Comment, Recipe, MealPlanRecipe
 
 class ArticleInline(admin.StackedInline):
     model = Article
@@ -18,3 +18,18 @@ class CommentAdmin(admin.ModelAdmin):
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'difficulty', 'servings', 'created_by', 'created_on', 'is_published', 'rating')
+    list_filter = ('category', 'difficulty', 'is_published', 'created_on')
+    search_fields = ('name', 'description', 'created_by__username')
+    ordering = ['-created_on']
+    readonly_fields = ('created_on', 'updated_on')
+
+class MealPlanRecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'meal_plan', 'scheduled_date')
+    list_filter = ('scheduled_date',)
+    search_fields = ('recipe__name', 'meal_plan__name')
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(MealPlanRecipe, MealPlanRecipeAdmin)
