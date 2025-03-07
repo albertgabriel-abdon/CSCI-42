ac8d205b008e0d9efbe253d1d21fd92b862656df
albertgabriel-abdon <166241481+albertgabriel-abdon@users.noreply.github.com>
2025-03-07 23:45:31 +0800

    cleaned up, added mealplan recipe stuff

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,27 @@
 from django.urls import path
-from .views import  (RecipeDetailView, ArticleListView, 
-                    ArticleDetailView, ArticleUpdateView, 
-                    ArticleDeleteView, ArticleCreateView, 
-                    ArticleGalleryView
+from .views import (RecipeDetailView, ArticleListView, 
+                    MealPlanListView, 
+                    MealPlanDetailView, MealPlanCreateView, 
+                    MealPlanUpdateView, MealPlanDeleteView,
+                    RecipeUpdateView, RecipeDeleteView,
+                    RecipeAddView, RecipeAddFormView
                     )
-
 urlpatterns = [
     path('blog/articles',ArticleListView.as_view(), name='article_site'),
-    path('blog/article/<int:pk>',ArticleDetailView.as_view(), name='article'),
-    path('blog/article/<int:pk>/edit', ArticleUpdateView.as_view(), name='update'),
-    path('blog/article/add', ArticleCreateView.as_view(), name='create'),
-    path('blog/article/<int:pk>/delete', ArticleDeleteView.as_view(), name='delete'),
-    path('blog/article/gallery', ArticleGalleryView.as_view(), name='gallery'),
     path('blog/recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+
+    path('blog/mealplans/', MealPlanListView.as_view(), name='mealplan_list'),
+    path('blog/mealplan/<int:pk>/', MealPlanDetailView.as_view(), name='mealplan_detail'),
+    path('blog/mealplan/create/', MealPlanCreateView.as_view(), name='mealplan_create'),
+    path('blog/mealplan/<int:pk>/update/', MealPlanUpdateView.as_view(), name='mealplan_update'),
+    path('blog/mealplan/<int:pk>/delete/', MealPlanDeleteView.as_view(), name='mealplan_delete'),
+
+    path('blog/mealplan/<int:pk>/add-recipe/', RecipeAddView.as_view(), name='recipe_add'),
+    path('blog/mealplan/<int:pk>/add-recipe-form/', RecipeAddFormView.as_view(), name='recipe_add_form'),
+    path('blog/recipe/<int:pk>/update/', RecipeUpdateView.as_view(), name='recipe_update'),
+    path('blog/recipe/<int:pk>/delete/', RecipeDeleteView.as_view(), name='recipe_delete'),
+
 ]
+
+
 app_name = 'blog'
