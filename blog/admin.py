3493a84d34f1202b24a9fbc30a34074ebb58e5ca
albertgabriel-abdon <166241481+albertgabriel-abdon@users.noreply.github.com>
2025-03-14 09:49:18 +0800

    all admin added so can ez track

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article, ArticleCategory, Comment, Recipe, MealPlanRecipe
+from .models import (Article, ArticleCategory, 
+                     Comment, Recipe, GroceryList,
+                     MealPlanRecipe, MealPlan, Ingredient, 
+                     RecipeIngredient, NutritionalInformation, 
+                     InventoryManager)
 
 class ArticleInline(admin.StackedInline):
     model = Article
@@ -26,10 +30,43 @@ class RecipeAdmin(admin.ModelAdmin):
     ordering = ['-created_on']
     readonly_fields = ('created_on', 'updated_on')
 
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'start_date', 'status', 'visibility')
+    list_filter = ('status', 'visibility', 'start_date')
+    search_fields = ('name', 'user__username')
+
 class MealPlanRecipeAdmin(admin.ModelAdmin):
     list_display = ('recipe', 'meal_plan', 'scheduled_date')
     list_filter = ('scheduled_date',)
     search_fields = ('recipe__name', 'meal_plan__name')
 
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit', 'estimated_expiration_date')
+    search_fields = ('name',)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity')
+    search_fields = ('recipe__name', 'ingredient__name')
+
+class NutritionalInformationAdmin(admin.ModelAdmin):
+    list_display = ('ingredient', 'calories', 'proteins', 'fat', 'carbs', 'fiber')
+    search_fields = ('ingredient__name',)
+
+class InventoryManagerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ingredient', 'quantity', 'is_buyer')
+    list_filter = ('is_buyer',)
+    search_fields = ('user__username', 'ingredient__name')
+
+class GroceryListAdmin(admin.ModelAdmin):
+    list_display = ('ingredient', 'user', 'status', 'updated_on')
+    list_filter = ('status', 'updated_on')
+    search_fields = ('ingredient__name', 'user__username')
+
+admin.site.register(GroceryList, GroceryListAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(MealPlan, MealPlanAdmin)
 admin.site.register(MealPlanRecipe, MealPlanRecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(NutritionalInformation, NutritionalInformationAdmin)
+admin.site.register(InventoryManager, InventoryManagerAdmin)
